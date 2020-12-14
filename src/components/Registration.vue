f1127205dadfefe76ade03fa57f9593da2d0d07b
Artem Bakiev <artembakiev@gmail.com>
2020-12-14 15:55:55 +0800

    install vuelidate

---
+++ src/components/Registration.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Тестовый запуск</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Registration.vue',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
